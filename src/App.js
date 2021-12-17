1af586d6cf8b93aae53cc4c238b095b73ed38843
“Magd <majdgalyl@gmail.com>
2021-12-17 22:20:22 +0200

    Translation Widget Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
 // import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 // const items = [
 //   {
@@ -35,24 +36,35 @@ const options = [
   },
 ];
 
+// ======================= //
+
 
 const App = () => {
 
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+  // const [selected, setSelected] = useState(options[0]);
+
+  //Body E.L
+  // const [showDropdown, setShowDropdown] = useState(true); 
 
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <button onClick={()=>{ setShowDropdown(!showDropdown) }}>Toggle Dropdown</button>
+     
+     {/* DropDown Body Event listener Test */}
+      {/* <button onClick={()=>{ setShowDropdown(!showDropdown) }}>Toggle Dropdown</button>
       {showDropdown ?
         <Dropdown 
         selected={selected}
         onSelectedChange={setSelected}
         options={options}
         /> : null
-      }
+      } */}
+
+      <Translate
+      
+      />
+
     </div>
   );
 };
